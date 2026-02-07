963cbb30c8777d64d474a55bf98b06b1c3a4adb1
giacomo cremona <giacomo.cremona@itsincom.it>
2026-02-07 16:02:24 +0100

    pulizia del codice

--- PcPartPickerProject/Program.cs
+++ PcPartPickerProject/Program.cs
@@ -24,57 +24,5 @@ app.UseAuthorization(); //esistono delle classi controller che dovrai mappare pe
 
 app.MapControllers();
 
-/*
-Build build = new Build();
-
-app.MapPost("/addCpu", ([FromBody] string name) =>
-{
-    Cpu c = DB.cpus.SingleOrDefault(c => c.model == name);
-    if (build.IsCompatible(c))
-    {
-        build.processor = c;
-        Console.WriteLine("cpu added because in compatible");
-        return Results.Ok();
-    }
-    else
-    {
-        Console.WriteLine("errore Cpu non compatibile");
-        return Results.BadRequest("cpu non compatibile");
-    }
-});
-
-app.MapPost("/addCpuCooler", ([FromBody] string name) =>
-{
-    CpuCooler c = DB.cpuCoolers.SingleOrDefault(c => c.model == name);
-    if (build.IsCompatible(c))
-    {
-        build.cpuCooler = c;
-        Console.WriteLine("cpu cooler added because in compatible");
-        return Results.Ok();
-        
-    }
-    else
-    {
-        Console.WriteLine("errore Cpu cooler non compatibile");
-        return Results.BadRequest("cpu cooler non compatibile");
-    }
-});
-
-app.MapGet("/getMobo", () =>
-{
-    return DB.motherboards;
-});
-
-app.MapGet("/getCpu", () =>
-{
-    return DB.cpus;
-});
-
-app.MapGet("/getCpuCooler", () =>
-{
-    return DB.cpuCoolers;
-});
-*/
-
 app.MapScalarApiReference();
 app.Run();
